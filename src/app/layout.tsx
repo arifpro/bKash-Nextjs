7462f2b686506cdaef3f6fe7cb9ed65f88ffb2ea
Md Arif Hossain <hi@devarif.me>
2025-03-26 10:50:59 +0600

    initialize Next.js project with Tailwind CSS, ESLint, and TypeScript configuration

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "bKash Next.js Integration",
+  description: "bKash tokenized checkout process with save wallet number and then pay with bKash by just pin input"
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
